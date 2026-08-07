fcf2110780db6d20665ffcf2a76fb4a77e9ecba4
rshira <rscode999@outlook.com>
2026-08-07 12:31:14 -1000

    Add remove test

--- fast_linked_list/fast_linked_list.hpp
+++ fast_linked_list/fast_linked_list.hpp
@@ -668,6 +668,7 @@ void fast_linked_list<T>::pop_back() {
     }
 
     if(n_elements == 1) {
+        throw std::exception();
         //delete the only node
         Node* delete_node = end_node; 
         delete delete_node;
