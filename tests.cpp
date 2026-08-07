81c9b185413c87816402fa942d2fcb60e719706b
rshira <rscode999@outlook.com>
2026-08-07 12:40:37 -1000

    add remove test

--- tests.cpp
+++ tests.cpp
@@ -27,8 +27,18 @@ void test_add() {
 }
 
 
+void test_remove() {
+    fast_linked_list<char> l;
+    
+    l.push_back('a');
+    l.pop_back();
+    assert_equals(0, l.size());
+
+    std::cout << "Tests passed" << std::endl;
+}
 
 
 int main() {
     test_add();
+    test_remove();
 }
